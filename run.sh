8bda8fdc8fe1ba15040ab54772e29c852a850e7a
Miaoyu Li <342031949@qq.com>
2025-10-15 20:20:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -25,18 +25,20 @@ expansion=5
 echo "Running evaluation for model: $model_id"
 echo "DEBUG mode: $DEBUG"
 
-declare -a tasks=("2needle_vision" "2needle_normal" "2needle_rev" "1needle")
+declare -a tasks=("2needle_vision" "2needle_normal" "2needle_rev" "1needle" "1needle_nc")
 declare -A prompts=(
   ["2needle_vision"]="prompts/test/test_prompt_2needle_vision.txt"
   ["2needle_normal"]="prompts/test/test_prompt_2needle_normal.txt"
   ["2needle_rev"]="prompts/test/test_prompt_2needle_rev.txt"
   ["1needle"]="prompts/test/test_prompt_1needle.txt"
+  ["1needle_nc"]="prompts/test/test_prompt_1needle.txt"
 )
 declare -A scripts=(
   ["2needle_vision"]="test_VLM_2needle_ID.py"
   ["2needle_normal"]="test_VLM_2needle_VG.py"
   ["2needle_rev"]="test_VLM_2needle_VG.py"
   ["1needle"]="test_VLM_1needle.py"
+  ["1needle_nc"]="test_VLM_1needle_NC.py"
 )
 
 for task in "${tasks[@]}"; do
